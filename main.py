bf73ba210b8cea857883b44b5f3111f4aabacbbd
uhmull <146273318+uhmull@users.noreply.github.com>
2024-12-21 23:22:24 -0800

    Update main.py
    code without timer being displayed and stopping the game

--- main.py
+++ main.py
@@ -1,84 +1,39 @@
-import turtle as trtl
+import turtle
 import random
 
-#my variables
 spot_color = "pink"
-spot_size = int("3")
-spot_shape = "circle"
-totalScore = 0
-font_setup = ("MonoLisa", 20)
-timer = 30
-counter_interval = 1000  
+spot_radius = 15  
+timer = 30 #seconds
+counter_interval = 1000
 timer_up = False
 colors = ['pink', 'purple', 'green', 'red', 'blue', 'orange']
 sizes = [0.5, 1, 1.5, 1.75, 2, 2.25]
-
-trtl = trtl.Screen()
-trtl.setup(width=300, height=300)
-spot = trtl
-spot.shape(spot_shape)
-spot.shapesize(spot_size)
+score = 0
+screen = turtle.Screen()
+screen.setup(width=300, height=300)
+screen.bgcolor("plum")
+spot = turtle.Turtle()
+spot.shape("circle") 
 spot.fillcolor(spot_color)
-score_writer = trtl.Turtle()
-counter = trtl.Turtle()
-trtl.bgcolor("mistyRose")
+spot.penup()
 
-def countdown():
-    global timer, timer_up
-    counter.clear()
-    if timer <= 0:
-        counter.write("Your time is up ):", font=font_setup)
-        timer_up = True
-    else:
-        counter.write("Timer: " + str(timer), font=font_setup)
-        timer -= 1
-        counter.getscreen().ontimer(countdown, counter_interval)
+def spot_clicked(x, y):
+    global timer_up, spot_radius
+    if not timer_up:
+      spot_color = random.choice(colors)
+      spot.fillcolor(spot_color)
+      spot.penup()
+      spot.hideturtle()
+      spot.goto(random.randint(-100, 100), random.randint(-100, 100))
+      spot_radius = random.choice(sizes) * 8
+      spot.showturtle()
+      update_score()
+      
 def update_score():
     global score
     score += 1
-    score_writer.clear()
-    score_writer.hideturtle()
-    score_writer.write(score, font=font_setup)
-def change_position():
-    new_xpos = random.randint(-200, 200)
-    new_ypos = random.randint(-150, 150)
-    spot.goto(new_xpos, new_ypos)
-    update_score()
-def spot_clicked(x, y):
-    global timer_up
-    if not timer_up: 
-        spot.penup()
-        spot.hideturtle()
-        change_position()
-        spot.showturtle()
-        spot.pendown()
-        new_color()
-        new_sizes()
-def new_color(): 
-    random_color = random.choice(colors)
-    spot.color(random_color)
-    spot.stamp()
-    spot.color(spot_color)
-def new_sizes(): 
-    newsize = random.choice(sizes)
-    spot.shapesize(newsize)
-  score = 0  
-def start_game(): 
-    global timer_up, score, timer
-    score = 0 
-    timer = 30
-    timer_up = False
-    score_writer.clear()
-    score_writer.write(score, font=font_setup)
-    countdown()
-
+    print("Score:" + str(score))
+    
 spot.onclick(spot_clicked)
-new_sizes()
-new_color()
-score_writer.penup()
-score_writer.goto(-100, 100)
-counter.penup()
-counter.goto(-250, 100)
-trtl.ontimer(countdown, counter_interval)
-start_game()
-trtl.mainloop()
+update_score()
+screen.mainloop()
